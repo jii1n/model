375450621e33dc1a39cb059107775c228e6c353c
jii1n <yj5596901@gmail.com>
2025-11-13 11:04:39 +0900

    Update and rename muse_gnn_cv_neural.py to ws_muse_gnn_cv_neural.py

--- models/ws_muse_gnn_cv_neural.py
+++ models/ws_muse_gnn_cv_neural.py
@@ -1,7 +1,7 @@
-#muse_gnn_cv_neural
+#ws_muse_gnn_cv_neural
 import torch
 from utils import cross_validate
-from neural_models.muse_GNN import MuSeGNN
+from neural_models.ws_muse_GNN import MuSeGNN
 import os
 import argparse
 parser = argparse.ArgumentParser()
@@ -11,24 +11,17 @@ from sklearn.metrics import confusion_matrix
 import numpy as np
 
 
-# Gene Expression settings
-# Options:
-# getmm_gene_expression_no_outliers.csv for getmm, no outliers
-# getmm_combat_seq_no_outliers_and_singles_gene_expression.csv for getmm, combat-seq, no singles, no outliers
-# combat_seq_age_corrected_getmm_gene_expression_no_outliers.csv
-# combat_seq_age_corrected_day_1_and_older_getmm_gene_expression_no_outliers.csv
-# combat_seq_age_corrected_L4_and_younger_getmm_gene_expression_no_outliers.csv
-# combat_seq_getmm_GO_filtered_gene_expression_no_singles_and_outliers.csv
+
 parser.add_argument('--expression_path', type=str,
-                    default="/data/bi1/common_datastore/getmm_combat_seq_no_outliers_and_singles_gene_expression.csv",
+                    default="../data/getmm_combat_seq_no_outliers_and_singles_gene_expression.csv",
                     help='path to gene expression data '
-                         '(default: /data/bi1/common_datastore/getmm_combat_seq_no_outliers_and_singles_gene_expression.csv)')
-parser.add_argument('--label_path', type=str, default="/data/bi1/common_datastore/labels.csv",
-                    help='path to labels (default: /data/bi1/common_datastore/labels.csv)')
-parser.add_argument('--age_path', type=str, default="/data/bi1/common_datastore/age.csv",
-                    help='path to age data (default: /data/bi1/common_datastore/age.csv)')
-parser.add_argument('--experiments_path', type=str, default="/data/bi1/common_datastore/sra_to_bioproject.csv",
-                    help='path to sra to bioproject mapping (default: /data/bi1/common_datastore/sra_to_bioproject.csv)')
+                         '(default: ../data/getmm_combat_seq_no_outliers_and_singles_gene_expression.csv)')
+parser.add_argument('--label_path', type=str, default="../data/labels.csv",
+                    help='path to labels (default: ../data/labels.csv)')
+parser.add_argument('--age_path', type=str, default="../data/age.csv",
+                    help='path to age data (default: ../data/age.csv)')
+parser.add_argument('--experiments_path', type=str, default="../data/sra_to_bioproject.csv",
+                    help='path to sra to bioproject mapping (default: ../data/sra_to_bioproject.csv)')
 # MLP parameters
 parser.add_argument('--mlp_hidden_dim', type=int, default=512,
                     help='embedding dimensions (default: 512)')
@@ -47,20 +40,20 @@ parser.add_argument('--epochs', type=int, default=100,
                     help='num training epochs (default: 100)')
 parser.add_argument('--seed', type=int, default=42, help="Seed")
 parser.add_argument('--eval_model_every', type=int, default=10,
-                    help="how often (in # of epochs) to evaluate the model (default: 10)") # 10epoch 진행될 때마다 검증 수행 
+                    help="how often (in # of epochs) to evaluate the model (default: 10)") 
 parser.add_argument('--train_MLP', action='store_true', help="train the pure MLP (default: False)")
-parser.add_argument('--mixsplit', action='store_true', help="perform a mixsplit as described in paper (default: False)") # 기본: non - mixsplit
-parser.add_argument('--num_folds', type=int, default=10, help="How many folds for cross validation (default: 10)") # 교차 검증시 10 folds
+parser.add_argument('--mixsplit', action='store_true', help="perform a mixsplit as described in paper (default: False)") 
+parser.add_argument('--num_folds', type=int, default=10, help="How many folds for cross validation (default: 10)") 
 # Data Filtering
 parser.add_argument('--aging_genes_only', action='store_true',
-                    help="train the model using aging genes only (default: False)") #노화 관련 유전자만 사용하여 모델 학습 
+                    help="train the model using aging genes only (default: False)") 
 # GNN parameters
-parser.add_argument('--k', type=int, default=22113, help="Number of nodes to keep after Sort Pooling (default: 22113)") # sort pooling 이후 유지할 노드의 수 설정 
-parser.add_argument('--num_backbone_layers', type=int, default=1, help="Number of GNN backbone layers (default 1)") # backbone layer 수
+parser.add_argument('--k', type=int, default=22113, help="Number of nodes to keep after Sort Pooling (default: 22113)") 
+parser.add_argument('--num_backbone_layers', type=int, default=1, help="Number of GNN backbone layers (default 1)") 
 parser.add_argument('--backbone_channels', type=int, default=1,
                     help="Number of backbone features / channels (default: 1)")
 parser.add_argument('--concat_input_graph', action='store_true',
-                    help="Concatenate input graph features (default: False)") # 입력 그래프의 특징을 모델 출력과 결합할지 
+                    help="Concatenate input graph features (default: False)") 
 parser.add_argument('--train_GNN', action='store_true', help="Train the pure GNN (default: False)")
 parser.add_argument('--test_mode', action='store_true', help="Run script in test mode without training")
 
@@ -92,7 +85,7 @@ torch.manual_seed(config.seed)
 
 
 if config.train_GNN:
-    muse_gnn_experiment_name = f"ver4_MuSeGNN-bblayers_{num_backbone_layers}-nchannels_{backbone_channels}-folds_{num_folds}" \
+    muse_gnn_experiment_name = f"ws_MuSeGNN-bblayers_{num_backbone_layers}-nchannels_{backbone_channels}-folds_{num_folds}" \
                                f"cat_input_{concat_input_graph}-k_{config.k}-lr_{learning_rate}-wd_{weight_decay}-bs_{batch_size}" \
                                f"-epchs_{epochs}-evalevery_{eval_model_every}-dpout_{config.dropout}" \
                                f"-mlpdim_{mlp_hidden_dim}-mlplayers_{num_mlp_layers}"
@@ -138,4 +131,5 @@ if config.train_GNN:
             epochs
         )
         
+
 
